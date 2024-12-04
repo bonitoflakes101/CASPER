8efba349520801b9bb20cc2753731e31c3f9a1a9
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2024-12-04 19:17:43 +0800

    basic lexer
    working on string literals + identifiers

--- CASPER/main.py
+++ CASPER/main.py
@@ -1,3 +1,15 @@
+from Lexer import Lexer
 
-if __name__ == "__main__":
-    pass
+
+LEXER_DEBUG: bool = True
+
+if __name__ == '__main__':
+    with open("tests/lexer.lime", "r") as f:
+        code: str = f.read()
+
+
+    if LEXER_DEBUG:
+        print("===== LEXER DEBUG =====")
+        debug_lex: Lexer = Lexer(source=code)
+        while debug_lex.current_char is not None:
+            print(debug_lex.next_token())
