8b5811d68505f11f5ee5542f95e64a794c36adff
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2024-12-04 20:05:59 +0800

    partial parser

--- CASPER/main.py
+++ CASPER/main.py
@@ -1,7 +1,10 @@
 from Lexer import Lexer
+from Parser import Parser
+from AST import Program
+import json
 
-
-LEXER_DEBUG: bool = True
+LEXER_DEBUG: bool = False
+PARSER_DEBUG: bool = True
 
 if __name__ == '__main__':
     with open("tests/lexer.lime", "r") as f:
@@ -13,3 +16,14 @@ if __name__ == '__main__':
         debug_lex: Lexer = Lexer(source=code)
         while debug_lex.current_char is not None:
             print(debug_lex.next_token())
+
+    l: Lexer = Lexer(source=code)
+    p: Parser = Parser(lexer=l)
+
+    if PARSER_DEBUG:
+        print("===== PARSER DEBUG =====")
+        program: Program = p.parse_program()
+
+        with open("debug/ast.json", "w") as f:
+            json.dump(program.json(), f, indent=4)
+        print("Wrote AST to debug/ast.json successfully")
