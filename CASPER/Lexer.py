9b0173352fc1ee8ad2c105f38b1bd098a47bdc21
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2025-02-05 01:02:38 +0800

    multiline comments

--- CASPER/Lexer.py
+++ CASPER/Lexer.py
@@ -372,22 +372,53 @@ class Lexer:
             
             # Token Creation for -, -=, --
             case '-':
-                if self.__peek_char() == '-':
-                    self.__read_char()
+                if self.__peek_char() == '-':  
+                    self.__read_char()  
+                    if self.__peek_char() == '-':  
+                        self.__read_char()  
+                        if self.__peek_char() == ' ':  
+                            self.__read_char()
+                                       
+                            comment_content = ""
+
+                            while True:
+                                self.__read_char()
+
+                                if self.current_char is None:
+                                    return self.__new_token(TokenType.ILLEGAL, comment_content.strip()) 
+                                                            
+                                if self.current_char == '-' and self.__peek_char() == '-':
+                                    self.__read_char() 
+                                    if self.__peek_char() == '-':  
+                                        self.__read_char()  
+
+                                        if self.__peek_char() in {'-'}:
+                                            comment_content += "---"  
+                                        else:
+                                            self.__read_char() 
+                                            return self.__new_token(TokenType.COMMENT, comment_content.strip())  
+
+                                comment_content += self.current_char
+                                
+                    # Handle `--` as `MINUS_MINUS`
                     if self.__is_valid_delimiter(TokenType.MINUS_MINUS):
                         self.__read_char()
                         return self.__new_token(TokenType.MINUS_MINUS, "--")
                     return self.__return_illegal_token()
+
                 elif self.__peek_char() == '=':
                     self.__read_char()
                     if self.__is_valid_delimiter(TokenType.MINUS_EQ):
                         self.__read_char()
                         return self.__new_token(TokenType.MINUS_EQ, "-=")
                     return self.__return_illegal_token()
-                # Single -
+                
+                # Single `-`
                 if self.__is_valid_delimiter(TokenType.MINUS):
                     return self.__consume_single_char_token(TokenType.MINUS)
+                
                 return self.__return_illegal_token()
+
 
             # Token Creation for *, *=, **
             case '*':
