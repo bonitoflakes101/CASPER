875c6d7be90d76599d75de5b6647bbec6c260955
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2025-02-06 13:15:10 +0800

    added null on del17

--- CASPER/Delimiters.py
+++ CASPER/Delimiters.py
@@ -46,7 +46,7 @@ class Delimiters:
     DEL14 = SPACE | ALPHA
     DEL15 = SPACE | NEWLINE | ALPHANUM | {'[', ']', '"', '$'} # L bracket
     DEL16 = SPACE | NEWLINE | {'[', ']', '$', '@', ',', '='} 
-    DEL17 = SPACE | {',', ')', ']'} # R bracket
+    DEL17 = SPACE | NULL | {',', ')', ']'} # R bracket
     DEL18 = {'('}
     DEL19 = {'['}
     identifier_del = SPACE | {'<', '-', '+', '*', '/', '%', '=', '<', '>', '!', '(', ')', '[', ']', ',' , '{', '}'} 
