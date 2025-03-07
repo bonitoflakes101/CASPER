61db99aa80695f260050dd703b52b24447c0a3d8
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2025-03-08 02:34:50 +0800

    improved UI

--- CASPER/static/index.js
+++ CASPER/static/index.js
@@ -1,36 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let codeEditor = document.getElementById("codeEditor");
-    let lineNumbers = document.getElementById("lineNumbers");
 
-    function updateLineNumbers() {
-        let lines = codeEditor.value.split("\n").length;
-        lineNumbers.innerHTML = Array.from({ length: lines }, (_, i) => i + 1).join("<br>");
+function updateLineNumbers() {}
+function syncScroll() {}
+
+console.log("index.js loaded. Setting up a custom Monaco theme...");
+
+
+function defineCasperMonacoTheme(monaco) {
+  monaco.editor.defineTheme('casperDark', {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [
+      { token: 'keyword', foreground: 'C586C0' },
+      { token: 'number', foreground: 'B5CEA8' },
+      { token: 'string', foreground: 'CE9178' },
+      { token: 'comment', foreground: '6A9955', fontStyle: 'italic' },
+      { token: 'variable', foreground: '9CDCFE' },
+      { token: 'identifier', foreground: '4FC1FF' },
+      { token: 'delimiter', foreground: 'FFFFFF' },
+      { token: 'operator', foreground: 'D4D4D4' }
+    ],
+    colors: {
+      'editor.background': '#1E1E1E',
+      'editor.foreground': '#CCCCCC',
+      'editorCursor.foreground': '#AEAFAD',
+      'editor.lineHighlightBackground': '#2C2C2C',
+      'editorLineNumber.foreground': '#858585',
+      'editor.selectionBackground': '#264F78'
     }
-
-    function syncScroll() {
-        lineNumbers.scrollTop = codeEditor.scrollTop;
-    }
-
-    function adjustHeight() {
-        codeEditor.style.height = codeEditor.scrollHeight + "px";
-    }
-
-    // Ensure everything is updated on page load
-    updateLineNumbers();
-    adjustHeight();
-
-    // Attach event listeners
-    codeEditor.addEventListener("input", () => {
-        updateLineNumbers();
-        adjustHeight();
-    });
-
-    codeEditor.addEventListener("scroll", syncScroll);
-});
+  });
+}
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".run-button").addEventListener("click", function () {
-        document.querySelector(".form-div").submit(); // Submit the form manually
-    });
-});
